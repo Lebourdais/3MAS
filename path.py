3dc323fda37fdfeb0f91134ed762df39b3ea5272
Lebourdais <lebourdais.martin@gmail.com>
2023-10-27 14:17:29 +0200

    cleaning

--- path.py
+++
@@ -1,3 +0,0 @@
-PATH_TO_PYANNOTE_DB = "/gpfswork/rech/wcp/commun/datasets/pyannote_db/database.yaml"
-PATH_TO_NOISE = "/gpfsdswork/dataset/MUSAN/noise/subdirectory/"
-PATH_TO_MUSIC = "/gpfsdswork/dataset/MUSAN/music/subdirectory"
