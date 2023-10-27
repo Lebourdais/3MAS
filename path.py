d27f821a74a5f18bcedb1aa04ca126eb570fc852
Lebourdais <lebourdais.martin@gmail.com>
2023-10-27 14:07:16 +0200

    first commit on right branch

---
+++ path.py
@@ -0,0 +1,3 @@
+PATH_TO_PYANNOTE_DB = "/gpfswork/rech/wcp/commun/datasets/pyannote_db/database.yaml"
+PATH_TO_NOISE = "/gpfsdswork/dataset/MUSAN/noise/subdirectory/"
+PATH_TO_MUSIC = "/gpfsdswork/dataset/MUSAN/music/subdirectory"
